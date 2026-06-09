bcf82f92aa47321b1d696460c390ebca333846f0
panlingnan <panlingnan@bytedance.com>
2026-06-09 15:59:56 +0800

    修改

--- main.tf
+++ main.tf
@@ -2,16 +2,22 @@ terraform {
   required_providers {
     volcenginecc = {
       source  = "volcengine/volcenginecc"
-      version = "~> 0.0.1-beta"
+      version = "~> 0.0.49"
     }
   }
 }
-resource "volcenginecc_vpc_subnet" "SubnetDemo" {
-  vpc_id      = "vpc-2bytdc3ac6z282dx0eg69pwdo"
-  zone_id     = "cn-guilin-a"
-  subnet_name = "subnet123"
-  description = "subnet123 description"
-  cidr_block  = "192.168.0.0/25"
+resource "volcenginecc_iam_user" "UserDemo" {
+  user_name   = "UserDemo"
+  description = "user description"
+  groups = [
+    "UserGroupDemo"
+  ]
+  policies = [
+    {
+      policy_name = "TOSReadOnlyAccess"
+      policy_type = "System"
+    }
+  ]
   tags = [
     {
       key   = "env"
